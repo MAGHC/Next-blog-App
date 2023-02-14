f89e8cc48212aa63525e1ec501e570709e3629e2
MAGHC <windowsapple95@gmail.com>
2023-02-14 20:12:25 +0900

    style: global style, tailwind.config, logo set

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{html,js,tsx,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: '#C4C4C4',
+        bright: '#E4E4E4',
+        darker: '#626262',
+        highLight: '#774B1E',
+        highLightSub: '#E8E0DD',
+      },
+    },
+    fontFamily: {
+      title: ['Poppins'],
+      body: ['Roboto'],
+      kor: ['Noto Sans KR'],
+    },
   },
   plugins: [],
 };
