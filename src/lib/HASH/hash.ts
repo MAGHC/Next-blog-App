14f5b451cbef1c985a46c3b4a3720c459bfab28c
MAGHC <windowsapple95@gmail.com>
2023-02-17 20:58:54 +0900

    feat: hash된 value 비교 기능

--- src/lib/HASH/hash.ts
+++ src/lib/HASH/hash.ts
@@ -1,7 +1,12 @@
-import { hash } from 'bcrypt';
+import { hash, compare } from 'bcrypt';
 
 export async function hashPw(pw: string) {
   const hashedPw = await hash(pw, 12);
 
   return hashedPw;
+}
+
+export async function verifyPw(pw: string, hashedPw: string) {
+  const isValid = await compare(pw, hashedPw);
+  return isValid;
 }
