07eaec7fb470ed5cb84aff01b3f0411c1030bdfc
MAGHC <windowsapple95@gmail.com>
2023-02-19 17:08:17 +0900

    fix: session 값에 따른 해더 동작 이상 해결

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="author" content="MAGHC"></meta>
+        <meta name="description" content="Portfolio for MAGHC" />
+        <meta name="next-head-count" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
