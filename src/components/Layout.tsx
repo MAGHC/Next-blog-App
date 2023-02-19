c9c907b5231d73f258b1ad7a0b7fe257c597d2a1
MAGHC <windowsapple95@gmail.com>
2023-02-19 17:12:17 +0900

    fix : Head 분리에 따른 컴포넌트 명 변경

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,10 @@
 import { Fragment } from 'react';
-import MainHeader from './MainHeader';
+import Nav from './Nav';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <Fragment>
-      <MainHeader></MainHeader>
+      <Nav></Nav>
       <main className="  mx-[8rem] h-full ">{children}</main>
     </Fragment>
   );
